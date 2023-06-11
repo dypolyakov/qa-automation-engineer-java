584708bfdc903adf5f9b4ec5faa89996b56f8253
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-06-11 20:28:22 +0500

    A second test was added to the SalaryService calcs

--- src/test/java/sprint6/topic2/unitTests/lesson11/SalaryServiceTest.java
+++ src/test/java/sprint6/topic2/unitTests/lesson11/SalaryServiceTest.java
@@ -13,4 +13,13 @@ public class SalaryServiceTest {
 
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void testSalaryOverLimit() {
+        SalaryService salaryService = new SalaryService();
+        int actual = salaryService.calculateSalary(1_000_000);
+        int expected = 50_000;
+
+        Assert.assertEquals(expected, actual);
+    }
 }
