308aca2cef7b045616efa546ed1d20e2fd485dea
Dmitry Polyakov <dmitryypolyakov@gmail.com>
2023-06-21 19:33:43 +0500

    Solution for second problem. Deserialization. Sprint 7/9 → Topic 2/4: Java. API → Lesson 4/8

---
+++ src/test/java/sprint7/topic2/javaAPI/lesson4/task2/User.java
@@ -0,0 +1,15 @@
+package sprint7.topic2.javaAPI.lesson4.task2;
+
+import sprint7.topic2.javaAPI.lesson4.task1.Data;
+
+public class User {
+    private Data data;
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+}
